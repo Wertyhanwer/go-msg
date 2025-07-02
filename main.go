3ba9846a7ef3b7dd43314bd3a8440bc4b1e0e03d
pablo <konndann02@mail.ru>
2025-07-02 17:07:29 +0300

    add: Basic working backend

--- main.go
+++ main.go
@@ -1,17 +1,22 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Messenger backend is up!")
+	http.HandleFunc("/message", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(map[string]string{"message": "Messenger backend is up!"})
 	})
 
-	fmt.Println("Server running at http://localhost:8080")
-	err := http.ListenAndServe(":8080", nil)
+	fs := http.FileServer(http.Dir("web"))
+	http.Handle("/", fs)
+
+	fmt.Println("Server running at http://localhost:30000")
+	err := http.ListenAndServe(":30000", nil)
 	if err != nil {
 		fmt.Println("Server error:", err)
 	}
